b8d4388393cf55c5f111db09a00f77848f6a047d
buddy <buddyglobalco@gmail.com>
2024-09-29 03:40:10 -0400

    commit made to the script file by link the html element to the script by add a variable to each element

---
+++ script.js
@@ -0,0 +1,16 @@
+var submit = document.querySelector(".inputcity")
+var searchcity = document.querySelector(".entervalue")
+var listhook = document.querySelector(".listhook")
+var API_KEY = "02282889aaeff515d61fdec7f58d0ff4";
+var asideclass = document.querySelector("#thirdcardclass")
+
+var date_weather = document.querySelector(".weatherdate")
+var imagtag  = document.querySelector(".imgtag")
+
+var weathercard = document.querySelector(".card")
+
+var secondaside = document.querySelector("#secondaside")
+
+var helemettag =document.querySelector("#daysforcast")
+
+var maindiv = document.querySelector("#maindiv")
